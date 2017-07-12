ee6312ea8945f90b1cfaf1fbcf597b27c1b96215
zhengxin <358581959@qq.com>
2017-07-12 13:31:13 +0800

    fix fun.py

--- copy.sh
+++ copy.sh
@@ -3,18 +3,20 @@
 ###########业务
 if [[ -d "/home/zhengxin/shared/业务语料" ]]; then
         if [[ -d "./dialogue_data" ]]; then
-                git rm ./dialogue_data -r
-                cp /home/zhengxin/shared/业务语料 ./dialogue_data -r
-                git add dialogue_data
-        else
-                echo 'dialogue_data no exist!'
+                git rm ./dialogue_data -fr
         fi
+        cp /home/zhengxin/shared/业务语料 ./dialogue_data -r
+        git add dialogue_data
 else
         echo '业务语料 no exist!'
 fi
 ###########营销
 if [[ -d "/home/zhengxin/shared/营销语料" ]]; then
-        cp /home/zhengxin/shared/营销语料 . -r
+        if [[ -d "./sale_data" ]]; then
+                git rm ./sale_data -fr
+        fi
+        cp /home/zhengxin/shared/营销语料 ./sale_data -r
+        git add sale_data
 else
         echo '营销语料 no exist!'
 fi
